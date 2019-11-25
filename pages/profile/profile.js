0740c9d2b26f44faf30f41519edb9663d97335bd
w597994146@163.com <w597994146@163.com>
2019-11-25 20:32:33 +0800

    tag04

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,41 +1,25 @@
-// pages/home/home.js
+// pages/profile/profile.js
 Page({
 
   /**
    * 页面的初始数据
    */
-  // Mustache语法
   data: {
-    name:'fuck',
-    age:18888,
-    students:[
-      { id: 1, name: "aa", age: 15 },
-      { id: 1, name: "bb", age: 16 },
-      {id:1,name:"cc",age:17},
-    ],
-    counter:0
+
   },
-  handleBtnClick(){
-    this.setData({
-      counter:this.data.counter+1
-    })
-  },
-  handleSubtraction(){
-    this.setData({
-      counter: this.data.counter -1
-    })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log('onload')
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+
   },
 
   /**
